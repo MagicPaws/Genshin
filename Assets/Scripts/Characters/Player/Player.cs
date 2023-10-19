bcaf2236e4b411544061b379cd8401401f59c248
MagicPaws <19239639479@192.net>
2023-10-20 04:26:42 +0800

    添加玩家控制器

--- Assets/Scripts/Characters/Player/Player.cs
+++ Assets/Scripts/Characters/Player/Player.cs
@@ -3,31 +3,41 @@ using Genshin.Characters.Player.StateMachines.Movement;
 using Genshin.Characters.Player.StateMachines.Movement.States;
 using Genshin.Characters.Player.StateMachines.Movement.States.Grounded;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Genshin.Characters.Player
 {
-    public class Player : MonoBehaviour
+    [RequireComponent(typeof(PlayerInput))]
+   public class Player : MonoBehaviour
     {
         private PlayerMovementStateMachine movementStateMachine;
-
+        public PlayerInput Input{get;private set;}
+        
         private void Awake()
         {
+            // 创建一个玩家移动状态机
             movementStateMachine = new PlayerMovementStateMachine();
+            // 获取玩家自身输入器
+            Input = GetComponent<PlayerInput>();
         }
 
         private void Start()
         {
+            // 开始时，将状态机设置为空闲状态
             movementStateMachine.ChangeState(movementStateMachine.IdlingState);
         }
 
         private void Update()
         {
+            // 更新状态机输入
             movementStateMachine.HandleInput();
+            // 更新状态机状态
             movementStateMachine.Update();
         }
 
         private void FixedUpdate()
         {
+            // 更新物理状态机状态
             movementStateMachine.PhysicsUpdate();
         }
     }
---
+++ Assets/Scripts/Characters/Player/Utilities/Input/PlayerInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Genshin.Characters.Player.Utilities.Input
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        //新输入系统
+        public PlayerInputActions InputActions { get; private set; }
+        //新输入系统中的PlayerActions的ActionMap
+        public PlayerInputActions.PlayerActions PlayerActions { get; private set; }
+        
+        private void Awake()
+        {
+            //初始化新输入系统
+            InputActions = new PlayerInputActions();
+            
+            PlayerActions = InputActions.Player;
+        }
+
+        // 当启用时调用
+        private void OnEnable()
+        {
+            // 启用输入动作
+            InputActions.Enable();
+        }
+
+        // 当禁用时调用
+        private void OnDisable()
+        {
+            // 禁用输入动作
+            InputActions.Disable();
+        }
+    }
+
+
+}
